8479231cd67ebdb7ffc9b7c6361a20dd4424b953
laurentC35 <38245508+laurentC35@users.noreply.github.com>
2022-03-17 15:02:16 +0100

    Add version number & update app description

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { IconButton, Tooltip, Typography } from '@mui/material';
 import './App.css';
 import { EnvForm } from 'components/settings';
@@ -6,27 +6,36 @@ import { Settings } from '@mui/icons-material';
 import { SaveMenu } from 'components/save/menu';
 import { Outlet } from 'react-router-dom';
 import { NewsUpdate } from 'components/newsUpdate';
+import { AppContext } from 'MainApp';
 
 function App() {
   const [settingsOpen, setSettingsOpen] = useState(false);
+  const { appVersion } = useContext(AppContext);
 
   return (
     <>
       <header className="as-header with-bottom-line">
         <div className="left">
           <Typography variant="h2">My Pogues</Typography>
-          <Typography variant="h6">Sauvegarde de questionnaire de Pogues</Typography>
+          <Typography variant="h6">
+            Sauvegarde et visualisation de questionnaire de Pogues
+          </Typography>
           <Typography>
-            <i>Les questionnaires sont sauvegardés dans votre navigateur</i>
+            <i>Les questionnaires et visualisations sont sauvegardés dans votre navigateur</i>
           </Typography>
         </div>
         <div className="right">
-          <SaveMenu />
-          <Tooltip title="Modifier la configuration">
-            <IconButton onClick={() => setSettingsOpen(true)}>
-              <Settings />
-            </IconButton>
-          </Tooltip>
+          <div className="icons">
+            <SaveMenu />
+            <Tooltip title="Modifier la configuration">
+              <IconButton onClick={() => setSettingsOpen(true)}>
+                <Settings />
+              </IconButton>
+            </Tooltip>
+          </div>
+          <div>
+            <i>{`Version ${appVersion}`}</i>
+          </div>
         </div>
       </header>
       <main>
